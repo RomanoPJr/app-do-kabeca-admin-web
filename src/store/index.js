fed101b596bed28aa0a8a76efe9d7b04364f0bfb
Caio Deambrosio <caio.deambrosio@gmail.com>
2020-03-16 18:24:17 -0300

    Hotfix Redux

--- src/store/index.js
+++ src/store/index.js
@@ -15,13 +15,13 @@ const rootReducer = combineReducers({
   statuteSuggestion
 });
 
-// const store = createStore(rootReducer, applyMiddleware(thunk));
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, applyMiddleware(thunk));
+// const store = createStore(
+//   rootReducer,
+//   compose(
+//     applyMiddleware(thunk),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
 
 export default store;
